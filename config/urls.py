2e1b8b0137a12c3bb1e40840f1b02336f91e6867
estaji <estaji.work@gmail.com>
2021-04-22 10:33:16 +0430

    Add media

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings # just for debug mode without a web server
+from django.conf.urls.static import static # just for debug mode without a web server
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogcore.urls')),
 ]
+
+# just for debug mode without a web server
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
