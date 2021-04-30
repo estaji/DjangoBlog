1fab662cbefc662dc0734b558ba90ccd77931085
estaji <estaji.work@gmail.com>
2021-04-30 11:57:39 +0430

    Add category page

--- blogcore/urls.py
+++ blogcore/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, detail, api
+from .views import home, detail, category, api
 
 app_name = "blog"
 urlpatterns = [
     path('', home, name="home"),
     path('article/<slug:slug>', detail, name="detail"),
+    path('category/<slug:slug>', category, name="category"),
     path('api',api, name="api"),
 ]
--- blogcore/views.py
+++ blogcore/views.py
@@ -18,6 +18,14 @@ def detail(request, slug):
 
     return render(request, "blog/detail.html", context)
 
+def category(request, slug):
+
+    context = {
+        "category": get_object_or_404(Category, slug=slug, status=True),
+    }
+
+    return render(request, "blog/category.html", context)
+
 def api(request):
 
     apidata = {
