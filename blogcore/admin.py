7c25e5ced7fd35864c96359aedfbae3157070afc
estaji <estaji.work@gmail.com>
2021-05-14 18:21:11 +0430

    Edit admin site header

--- blogcore/admin.py
+++ blogcore/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Article, Category
 
+# Admin header change
+admin.site.site_header = "وبلاگ جنگو"
+
 def make_published(modeladmin, request, queryset):
     rows_updated = queryset.update(status='p')
     if rows_updated == 1:
